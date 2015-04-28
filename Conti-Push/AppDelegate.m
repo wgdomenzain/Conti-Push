88bd08fbf1f60f6ffb4046b9d2dce8a981440023
Walter Gonzalez Domenzain <walter.gonzalez@smartplace.mx>
2015-04-28 11:32:57 -0500

    Initial configuration (not working yet)

--- Conti-Push/AppDelegate.m
+++ Conti-Push/AppDelegate.m
@@ -7,8 +7,11 @@
 //
 
 #import "AppDelegate.h"
+#import <Pushwoosh/PushNotificationManager.h>
 
-@interface AppDelegate ()
+@interface AppDelegate ()<PushNotificationDelegate> {
+    PushNotificationManager *pushManager;
+}
 
 @end
 
@@ -16,7 +19,20 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    //-----------PUSHWOOSH PART-----------
+    // set custom delegate for push handling, in our case - view controller
+    pushManager = [PushNotificationManager pushManager];
+    pushManager.delegate = self;
+    
+    // handling push on app start
+    [[PushNotificationManager pushManager] handlePushReceived:launchOptions];
+    
+    // make sure we count app open in Pushwoosh stats
+    [[PushNotificationManager pushManager] sendAppOpen];
+    
+    // register for push notifications!
+    [[PushNotificationManager pushManager] registerForPushNotifications];
     return YES;
 }
 
@@ -122,6 +138,25 @@
             abort();
         }
     }
+}
+
+// system push notification registration success callback, delegate to pushManager
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    [[PushNotificationManager pushManager] handlePushRegistration:deviceToken];
+}
+
+// system push notification registration error callback, delegate to pushManager
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+    [[PushNotificationManager pushManager] handlePushRegistrationFailure:error];
+}
+
+// system push notifications callback, delegate to pushManager
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    [[PushNotificationManager pushManager] handlePushReceived:userInfo];
+}
+
+- (void) onPushAccepted:(PushNotificationManager *)pushManager withNotification:(NSDictionary *)pushNotification {
+    NSLog(@"Push notification received");
 }
 
 @end
